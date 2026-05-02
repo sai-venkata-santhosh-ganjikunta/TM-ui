f2d70d4b5e016bf653c4bcddc3dedb12b4162469
SAI VENKATA SANTHOSH GANJIKUNTA <gsvsanthosh@gmail.com>
2026-05-02 11:48:52 +0530

    URL change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'tm-api-production.up.railway.app',
+  baseURL: 'https://tm-api-production.up.railway.app',
 });
 
 api.interceptors.request.use((config) => {
